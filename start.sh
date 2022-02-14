b2470427abfbf580c8c1bcf4a2e07a616cd5cad8
Nuno Sousa <nuno.sousa@uphold.com>
2022-02-14 15:47:12 +0000

    Only regenerate certificates if needed

--- start.sh
+++ start.sh
@@ -2,11 +2,25 @@
 
 # Install and convert a let's encrypt certificate, if provided.
 if [ -d "/etc/certificates" ]; then
-  rm -rf /var/lib/unifi-video/{certificates,keystore,ufv-truststore}
-  mkdir -p /var/lib/unifi-video/certificates/
+  newHash=$(md5sum /etc/certificates/privkey.pem | cut -f1 -d ' ')
 
-  openssl x509 -outform der -in "/etc/certificates/fullchain.pem" -out /var/lib/unifi-video/certificates/ufv-server.cert.der
-  openssl pkcs8 -topk8 -inform PEM -outform DER -nocrypt -in "/etc/certificates/privkey.pem" -out /var/lib/unifi-video/certificates/ufv-server.key.der
+  # Retrieve existing hash.
+  if [ -f /var/lib/unifi-video/certificates/md5.lock ]; then
+    existingHash=$(cat /var/lib/unifi-video/certificates/md5.lock)
+  fi
+
+  # If hash is not the same, then update the certificates.
+  if [ "${newHash}" != "${existingHash}" ]; then
+    rm -rf /var/lib/unifi-video/{cam-keystore,certificates,keystore,ufv-truststore}
+    mkdir -p /var/lib/unifi-video/certificates/
+
+    openssl x509 -outform der -in "/etc/certificates/fullchain.pem" -out /var/lib/unifi-video/certificates/ufv-server.cert.der
+    openssl pkcs8 -topk8 -inform PEM -outform DER -nocrypt -in "/etc/certificates/privkey.pem" -out /var/lib/unifi-video/certificates/ufv-server.key.der
+
+    chown -R $PUID:$PGID /var/lib/unifi-video/certificates/
+
+    echo "$newHash" > /var/lib/unifi-video/certificates/md5.lock
+  fi
 fi
 
 exec /run.sh
